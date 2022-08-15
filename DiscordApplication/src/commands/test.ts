8636a25496355ba5bdc9eb4ef8db8a8c70b70659
laycookie <80195126+laycookie@users.noreply.github.com>
2022-08-15 06:30:37 -0700

    Adding example command file

--- DiscordApplication/src/commands/test.ts
+++ DiscordApplication/src/commands/test.ts
@@ -1,7 +1,8 @@
 const name = "test";
 const description = "Sends test in the chat.";
+const permissions: string[] = [];
 const execute = () => {
     console.log("test!");
 };
 
-export { name, description, execute };
+export { name, description, permissions, execute };
