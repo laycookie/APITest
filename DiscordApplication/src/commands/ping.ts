aea07169b4a0fffca1f4ce3a9db031b31bb863b6
laycookie <80195126+laycookie@users.noreply.github.com>
2022-08-20 20:47:49 -0700

    Database setup in main

--- DiscordApplication/src/commands/ping.ts
+++ DiscordApplication/src/commands/ping.ts
@@ -1,5 +1,4 @@
-import { ChatInputCommandInteraction, CacheType } from "discord.js";
-
+// command code
 const name = "ping";
 const description = "Sends ping in the chat.";
 const permissions: string[] = [];
--- DiscordApplication/src/index.ts
+++ DiscordApplication/src/index.ts
@@ -1,6 +1,22 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import "dotenv/config";
+import { MongoClient } from "mongodb";
 import { commandsCode } from "./deploy-commands";
+import newServerDeployDefault from "./newServerDeployDefault.json";
+
+// Database setup
+const uri = `mongodb+srv://${process.env.DBUserName}:${process.env.Password}@cluster0.uzvgv.mongodb.net/?retryWrites=true&w=majority`;
+const DBClient = new MongoClient(uri);
+const database = DBClient.db("DiscordServerList");
+
+function ifDBExistElseCreate(serverName: string) {
+    database.listCollections({ name: serverName }).next((err, collinfo) => {
+        if (err) throw err;
+        else if (collinfo === null) {
+            database.collection(serverName).insertOne(newServerDeployDefault);
+        }
+    });
+}
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -8,19 +24,38 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
     console.log("Ready!");
+
+    client.on("interactionCreate", async (interaction) => {
+        if (!interaction.isChatInputCommand()) return;
+
+        const { commandName } = interaction;
+
+        for (const command of commandsCode) {
+            if (commandName === command.name) {
+                command.execute(interaction);
+                break;
+            }
+        }
+    });
+
+    // Verifying that all servers are in the database
+    const guildsArr: string[] = [];
+    client.guilds.cache.forEach((guild) => {
+        guildsArr.push(guild.name);
+    });
+    for (const guild of guildsArr) {
+        ifDBExistElseCreate(guild);
+    }
 });
 
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
+// On bot joining a server
+client.on("guildCreate", (guild) => {
+    console.log(
+        `Hello, I'm LMAOBOT. Thanks for inviting me, here are a list of all my commands! :alien:`,
+    );
 
-    const { commandName } = interaction;
-
-    for (const command of commandsCode) {
-        if (commandName === command.name) {
-            command.execute(interaction);
-            break;
-        }
-    }
+    const newServer = guild;
+    ifDBExistElseCreate(newServer.name);
 });
 
 // Login to Discord with your client's token
