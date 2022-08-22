e274128667d7c944a2e590eb45fa51f6d1b3e7f1
laycookie <80195126+laycookie@users.noreply.github.com>
2022-08-21 17:37:04 -0700

    Made ping read from the DB.

--- DiscordApplication/src/commands/ping.ts
+++ DiscordApplication/src/commands/ping.ts
@@ -1,3 +1,11 @@
+import { ChatInputCommandInteraction, CacheType } from "discord.js";
+import { MongoClient } from "mongodb";
+
+// database setup
+const uri = `mongodb+srv://${process.env.DBUserName}:${process.env.Password}@cluster0.uzvgv.mongodb.net/?retryWrites=true&w=majority`;
+const DBClient = new MongoClient(uri);
+const database = DBClient.db("DiscordServerList");
+
 // command code
 const name = "ping";
 const description = "Sends ping in the chat.";
@@ -5,11 +13,25 @@ const permissions: string[] = [];
 const execute = async (interaction: ChatInputCommandInteraction<CacheType>) => {
     const time = new Date();
     await interaction.deferReply();
-    await interaction.editReply("Pong !");
+
+    let pingResFromDb: string | null = null;
+    await database
+        .collection("LAYSCORD")
+        .find()
+        .forEach((json) => {
+            pingResFromDb = json.commands.pingRes;
+            if (pingResFromDb === null) {
+                interaction.editReply(
+                    "Something went wrong with the database.",
+                );
+            } else {
+                interaction.editReply(pingResFromDb);
+            }
+        });
 
     try {
         const ms = time.getMilliseconds() - new Date().getMilliseconds();
-        await interaction.editReply(`Pong !\n${ms} ms`);
+        await interaction.editReply(`${pingResFromDb} \n${ms} ms`);
     } catch {
         await interaction.editReply("PingCommand is unavailable.");
         throw new Error("Something dum had happened in ping.ts");
